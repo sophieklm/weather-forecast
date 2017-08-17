429c422e8659e6e96be5cb17f427b53e687d7059
sophieklm <sklm@live.co.uk>
2017-08-17 12:54:34 +0100

    adds axios

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,9 +1,16 @@
+import axios from 'axios';
+
 const API_KEY = '188d4bfce0208cc4d0252a7dc677a8f6';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
-export function fetchWeather() {
+export function fetchWeather(city) {
+  const url = `${ROOT_URL}&q=${city},uk`;
+  const request = axios.get(url);
+
   return {
-    type: FETCH_WEATHER
+    type: FETCH_WEATHER,
+    payload: request
   };
 }
