0adad3e068ecdd4b9882ab38988994800670c259
sophieklm <sklm@live.co.uk>
2017-08-17 11:58:19 +0100

    adds searchbar

---
+++ src/containers/search_bar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Component } from 'react';
+
+export default class SearchBar extends Component {
+  render() {
+    return (
+      <form className="input-group">
+        <input />
+      </form>
+    );
+  }
+}
